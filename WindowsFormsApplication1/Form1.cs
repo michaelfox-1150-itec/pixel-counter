fd1fe876ffe44ae22e4fc06c9269143d7fef127b
dog <dog@dog.com>
2016-10-18 20:41:03 -0500

    Color change, name change

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.Chartreuse;
         }
 
         private void Form1_Load(object sender, EventArgs e)
