ec7882219942cd05ca6031ee762ab888c772cd2a
Michael Fox <vr8834tz@go.minneapolis.edu>
2016-10-18 20:03:42 -0500

    This is a commit. GUIIIIIIIIIIIIIIIIII

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pixelsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pixelsLabel
+            // 
+            this.pixelsLabel.AutoSize = true;
+            this.pixelsLabel.Location = new System.Drawing.Point(0, 0);
+            this.pixelsLabel.Name = "pixelsLabel";
+            this.pixelsLabel.Size = new System.Drawing.Size(96, 13);
+            this.pixelsLabel.TabIndex = 0;
+            this.pixelsLabel.Text = "Pixels in Window =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pixelsLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label pixelsLabel;
     }
 }
 
